f192b14b1dacd5f23a440debfd2de305fa61825c
Travis Witherington <twitherington@covermymeds.com>
2018-02-02 11:04:15 -0600

    added ability to add friends and track their stocks, and homepage image

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,39 @@
 class UsersController < ApplicationController
   def my_portfolio
-    @user = current_user
     @user_stocks = current_user.stocks
+    @user = current_user
   end
 
   def my_friends
+    @friendships = current_user.friends
+  end
 
+  def search
+    if params[:search_param].blank?
+      flash.now[:danger] = 'Please enter information into the search.'
+    else
+      @users = User.search(params[:search_param])
+      @users = current_user.except_current_user(@users)
+      flash.now[:danger] = 'No users match this search criteria.' if @users.blank?
+    end
+    respond_to do |format|
+      format.js { render partial: 'friends/result' }
+    end
+  end
+
+  def add_friend
+    @friend = User.find(params[:friend])
+    current_user.friendships.build(friend_id: @friend.id)
+    if current_user.save
+      flash[:success] = 'Friend was successfully added.'
+    else
+      flash[:danger] = 'There was something wrong with the friend request.'
+    end
+    redirect_to my_friends_path
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @user_stocks = @user.stocks
   end
 end
